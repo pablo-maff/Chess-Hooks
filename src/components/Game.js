aaa355314cd88fa1e0bd74a3b22e104e5719c89d
pablo-maff <pablo.maff.076@gmail.com>
2022-03-22 14:05:52 +0100

    Movement pickle eaten, thanks Kalle!

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,36 +2,47 @@ import { useEffect, useState } from "react"
 import { initializeBoard } from "../tools"
 import Board from "./Board"
 
-const processMove = (obj, from, to) => {
-  const newPos = Object.assign(obj[to].piece, obj[from].piece)
-  obj = Object.assign([...obj], newPos)
-  //obj[from].piece.player = null
-  //obj[from].piece.type.name = null
-  //obj[from].piece.type.render = null
+const processMove = (move, from, to) => {
+  const piece = move[from].piece
 
-  return obj
+  // REMEMBER!!! returns a copy of the object with the specified values changed
+  move[from] = {
+    ...move[from],
+    piece: {
+      player: null,
+      type: {
+        name: null,
+        render: null
+      }
+    },
+  }
+
+  move[to] = {
+    ...move[to],
+    piece,
+  }
+
+  return move
 }
 
 const Game = () => {
   const [squares, setSquares] = useState([]) 
   const [selected, setSelected] = useState([])
-  //console.log('selectedState',selected);
-
 
   useEffect(() => {
     setSquares(initializeBoard)
   }, [])
 
-  // if contains a piece select square then select another square to move the piece, else null
+  // if contains a piece select square then select another square to move the piece
   const movePiece = (squareId, from = selected[0], to = selected[1]) => {
-    const moves = [...squares]
+    const currentBoard = [...squares]
     if (selected.length < 2) setSelected(selected.concat(squareId))
 
     // if array is full, make move and clear state
+    
+    // Next TODO make the move after 2 clicks, not 3 like now and add movement rules
     if (selected.length === 2) {
-      setSquares(processMove(moves, from, to))
-      console.log('from',moves[from].piece);
-      console.log('to', moves[to].piece);
+      setSquares(processMove(currentBoard, from, to))
       setSelected([])
     } 
   }
