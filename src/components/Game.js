0c7a6322115291474ceae12ff4d7da7912e83ce4
pablo-maff <pablo.maff.076@gmail.com>
2022-03-24 23:26:25 +0100

    Pawn movement finished, fixed bugs of jumping over other pieces when they are on initial positions and being able to destroy own player pieces

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,55 +2,83 @@ import { useState } from "react"
 import { initializeBoard, processMove } from "../tools"
 import Board from "./Board"
 
-function range(size, startAt = 0) {
-  return [...Array(size).keys()].map(i => i + startAt);
-}
+const range = (size, startAt = 0) =>
+  [...Array(size).keys()].map(i => i + startAt);
+
 
 const isMovePossible = (board, from, to) => {
-  const piece = board[from].piece.type
-  const player = board[from].piece.player
-  const destination = board[to].piece.type
+  const pieceInOrigin = board[from].piece.type
+  const playerInOrigin = board[from].piece.player
+  const pieceInDestination = board[to].piece.type
+  const playerInDestination = board[to].piece.player
 
   const isDestOccupied = () => {
-    if (destination === null) {
+    if (pieceInDestination === null) {
       return false
     }
     return true
   }
   
-  if (piece === 'pawn') {
+  // If piece is not a horse, it can't jump over other pieces!
+  // This implementation works for pawns, for other pieces it will need to iterate over the full path
+  const isPathClean = (path) => {
+    if (board[path].piece.type === null) {
+      return true
+    }
+    return false 
+  }
+
+  const isEnemyPiece = () => {
+    if (playerInOrigin !== playerInDestination) return true
+    
+    return false
+  }
+
+  const isPawnMovePossible = () => {
     const initialPawnPositions = {
       'white': range(8, 48),
       'black': range(8, 8)
     }
+
+    const whiteInitialPath = [from - 8]
+    const blackInitialPath = [from + 8]
+
     // NEXT TODO WRITE TESTS for movements of all pieces!
     // BUG can defeat piece of the same player
-    const isPawnMovePossible = () => {
-      if (player === 'white') {
-        if (((to === from - 8) || (to === from - 16 && initialPawnPositions[player].indexOf(from) !== -1)) && !isDestOccupied()) {
-          return true
-        }
-        else if ((to === from - 7 || to === from - 9) && isDestOccupied()) {
-          return true
-        } 
-      } 
-      if (player === 'black') {
-        if (((to === from + 8) || (to === from + 16 && initialPawnPositions[player].indexOf(from) !== -1)) && !isDestOccupied()) {
-          return true
-        }
-        else if ((to === from + 7 || to === from + 9) && isDestOccupied()) {
-          return true
-        }
+    if (playerInOrigin === 'white') {
+      if (((to === from - 8) || 
+        (to === from - 16 && initialPawnPositions[playerInOrigin].includes(from)))
+        && isPathClean(whiteInitialPath) && !isDestOccupied()) {
+        return true
       }
-      return false
+      else if ((to === from - 7 || to === from - 9) && isEnemyPiece() && isDestOccupied()) {
+        return true
+      }
     }
+
+    if (playerInOrigin === 'black') {
+      if (((to === from + 8) ||
+      (to === from + 16 && initialPawnPositions[playerInOrigin].includes(from)))
+       && isPathClean(blackInitialPath) && !isDestOccupied()) {
+        return true
+      }
+      else if ((to === from + 7 || to === from + 9) && isEnemyPiece() && isDestOccupied()) {
+        return true
+      }
+    }
+    return false
+  }
+
+  if (pieceInOrigin === 'pawn') {
     return isPawnMovePossible()
+
+
   }
   // TODO: Define movement rules of the rest of the pieces
 }
 
 const Game = () => {
-  const [board, setBoard] = useState(initializeBoard()) 
+  const [board, setBoard] = useState(initializeBoard())
   const [selected, setSelected] = useState([])
 
   // Move piece
@@ -63,9 +91,9 @@ const Game = () => {
       setSelected([])
     }
     else setSelected([])
-  } 
+  }
   // if contains a piece select square then select another square to move the piece
-  const movePiece = (boardId, from = selected[0], to = selected[1]) => {
+  const movePiece = (boardId, from = selected[0]) => {
     if (selected.length < 2) {
       // select to
       if (from) {
@@ -79,12 +107,12 @@ const Game = () => {
   }
 
   return (
-     <div className="game-board">
-       <Board 
-         board={board}
-         selectSquare={movePiece}
-       />
-     </div>
+    <div className="game-board">
+      <Board
+        board={board}
+        selectSquare={movePiece}
+      />
+    </div>
   )
 }
 
