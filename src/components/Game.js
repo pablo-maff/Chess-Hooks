f216db7a227705f55b308a76edec7e564c5e7987
pablo-maff <pablo.maff.076@gmail.com>
2022-03-27 15:36:51 +0200

    Tide up of useEffect

--- src/components/Game.js
+++ src/components/Game.js
@@ -9,36 +9,38 @@ const Game = () => {
   const [checkMate, setCheckMate] = useState(false)
   const [turn, setTurn] = useState('white')
   
-  console.log('turn',turn);
   if (checkMate) console.log('GAME OVER!');
 
   useEffect(() => {
+    console.log('EFFECT!');
     const from = selected[0]
     const to = selected[1]
-
     const kingPos = getKingPosition(board)
+    const canMove = isMovePossible(board, from, to, turn, checkMate)
+
     if (kingPos.length < 2) {
       setCheckMate(true)
     }
 
-    if (isMovePossible(board, from, to, turn, checkMate)){
+    if (canMove){
       setBoard(processMove(board, from, to))
       setSelected([])
       setTurn(turn === 'white' ? 'black' : 'white')
     }
-    if (selected.length === 2) setSelected([])
+    else if (selected.length === 2) setSelected([])
+
   }, [board, selected, turn, checkMate])
 
   // if contains a piece select square then select another square to move the piece
   const selectPiecePath = (boardId) => {
+    // select to
     if (selected.length < 2) {
-      // select to
-        setSelected(selected.concat(boardId))
+      setSelected(selected.concat(boardId))
     }
-      // select from
-      else if (board[boardId].piece.type !== null) {
-        setSelected(selected.concat(boardId))
-      }
+    // select from
+    else if (board[boardId].piece.type !== null) {
+      setSelected(selected.concat(boardId))
+    }
     
   }
 
