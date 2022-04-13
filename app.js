3e9a27714b5a5386f786f92d5bf29eed6304b01b
pablo-maff <pablo.maff.076@gmail.com>
2022-04-13 13:20:20 +0200

    /version now fetches the current version from package.json

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.get('/health', (req, res) => {
 })
 
 app.get('/version', (req, res) => {
-  res.send('1') // change this string for the proper version number
+  res.send(process.env.npm_package_version)
 })
 
 app.listen(PORT, () => {
